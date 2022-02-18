ad38cd131d688376e24c70e393b7f958ca9bd1ed
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-18 11:22:13 +0530

    more get methods added

--- index.js
+++ index.js
@@ -3,21 +3,28 @@ const express = require('express');
 const app = express();
 const port = 9999;
 
+// app.get(arg1, arg2);
+// app.get('url', () => {});
+
+// http://localhost:9999
 app.get('/', (request, response) => {
     console.log(`Welcome`);
     response.send(`Welcome to TechM NodeJS App at ${port}`);
 });
 
+// http://localhost:9999/home
 app.get('/home', (request, response) => {
     console.log(`Welcome Home`);
     response.send(`Welcome to TechM NodeJS App home`);
 });
 
+// http://localhost:9999/contact
 app.get('/contact', (request, response) => {
     console.log(`Welcome contact`);
     response.send(`Welcome to TechM NodeJS App contact`);
 });
 
+// http://localhost:9999/about
 app.get('/about', (request, response) => {
     console.log(`Welcome About`);
     response.send(`Welcome to TechM NodeJS App about`);
